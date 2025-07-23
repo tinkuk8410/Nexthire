2e927c2281174ec7c0b81a68f98998f64e23b4de
tinkuk8410 <tikeshwar8410@gmail.com>
2025-07-23 17:24:04 +0530

    align text in centre

--- src/component/home/index.jsx
+++ src/component/home/index.jsx
@@ -9,9 +9,10 @@ const Home = ()=>{
 
       <Header/>
 
-      <div className='d-flex justify-content-center align-items-center w-100 text-center'>
-        <h1>"Connecting Talent with Opportunity – Start Your Journey Today!"</h1>
+      <div className='d-flex justify-content-center align-items-center w-100 text-center' style={{ height: '100vh' }}>
+        <h1 className='text-center'>"Connecting Talent with Opportunity – Start Your Journey Today!"</h1>
       </div>
+
 
       <marquee behavior='fast' className='marqee-cont' >Let secure your career and find job with us. We'll never dissappoint you. </marquee>
             
